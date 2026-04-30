cab427f7e182af66213cc45af876b7157bc02fb8
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-04-30 10:51:12 +0800

    Initial Commit

---
+++ IOS-WaifuIm-Client/IOS_WaifuIm_ClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_WaifuIm_ClientApp.swift
+//  IOS-WaifuIm-Client
+//
+//  Created by Muhammad Akbar Reishandy on 30/04/26.
+//
+
+import SwiftUI
+
+@main
+struct IOS_WaifuIm_ClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
