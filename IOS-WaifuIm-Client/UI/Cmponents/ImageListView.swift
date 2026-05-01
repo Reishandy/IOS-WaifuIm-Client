3ca1d7f939ef3b55d6b1c3fc8e9b3c9eab47e025
Muhammad Akbar Reishandy <akbar@reishandy.id>
2026-05-01 12:39:09 +0800

    extracted image list commponent

---
+++ IOS-WaifuIm-Client/UI/Cmponents/ImageListView.swift
@@ -0,0 +1,18 @@
+//
+//  ImageListView.swift
+//  IOS-WaifuIm-Client
+//
+//  Created by Muhammad Akbar Reishandy on 01/05/26.
+//
+
+import SwiftUI
+
+struct ImageListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImageListView()
+}
